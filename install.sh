92eef9d452fd9e71024eaa6bd45d6bd570d49249
Ryan McHugh <ryanmmch@gmail.com>
2020-04-13 21:43:20 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+#
+# File: install.sh
+# Description: Bash executable for setting up all dependencies
+# Project: Carberry Pi
+# Author: Ryan McHugh
+# Year: 2020
+#
 
 PACKAGES= "qml Python3-pyqt5 Python3-pyqt5.qtquick Qml-module-qtquick* Python3-setuptools"
 
 sudo apt-get install "$PACKAGES" 
 
-export CARLOC=`pwd`/src/
+sudo echo "export CARLOC=`pwd`/src/" >> ./bashrc
